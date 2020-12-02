ff9c2f3a663fc68eccbccd005b1dfdec524e0ea5
Steven R <anguelr12@hotmail.com>
2020-12-01 22:15:02 -0500

    terraform console - apply -refresh=false

--- terraform/01-terrform-basics/main.tf
+++ terraform/01-terrform-basics/main.tf
@@ -9,3 +9,11 @@ resource "aws_s3_bucket" "my_s3_bucket" {
       enabled = true
     }
 }
+
+output "my_s3_bucket_versioning" {
+  value = aws_s3_bucket.my_s3_bucket.versioning[0].enabled
+}
+
+output "my_s3_bucket_complete_details" {
+  value = aws_s3_bucket.my_s3_bucket
+}
