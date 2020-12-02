15e2b89c5d4817c41322dd470653956c66a37391
Steven R <anguelr12@hotmail.com>
2020-12-01 22:35:50 -0500

    creating aws iam user

--- terraform/01-terrform-basics/main.tf
+++ terraform/01-terrform-basics/main.tf
@@ -10,10 +10,20 @@ resource "aws_s3_bucket" "my_s3_bucket" {
     }
 }
 
+resource "aws_iam_user" "my_iam_user" {
+    name = "my_iam_user_abc"
+}
+
+
+
 output "my_s3_bucket_versioning" {
   value = aws_s3_bucket.my_s3_bucket.versioning[0].enabled
 }
 
 output "my_s3_bucket_complete_details" {
   value = aws_s3_bucket.my_s3_bucket
+}
+
+output "my_iam_user_complete_details" {
+  value = aws_iam_user.my_iam_user
 }
