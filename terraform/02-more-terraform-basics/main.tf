331d9206711da483a97fde65b01057253dcc4ca5
Steven R <anguelr12@hotmail.com>
2020-12-08 14:24:05 -0500

    fmt, show and console

--- terraform/02-more-terraform-basics/main.tf
+++ terraform/02-more-terraform-basics/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    region = "us-east-1"
-    version = "~> 3.18.0"
+  region  = "us-east-1"
+  version = "~> 3.18.0"
 }
 
 //Multiple users
 resource "aws_iam_user" "my_iam_users" {
-    count = 3
-    name = "my_iam_user_${count.index}"
+  count = 3
+  name  = "my_iam_user_${count.index}"
 }
