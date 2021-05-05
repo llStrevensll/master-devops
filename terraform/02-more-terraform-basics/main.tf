5338efa5cddfc08623dec6c305dd2cb2c638af06
llStrevensll <anguelr12@hotmail.com>
2021-05-05 05:38:06 -0500

    provider version, outputs info

--- terraform/02-more-terraform-basics/main.tf
+++ terraform/02-more-terraform-basics/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 variable "iam_user_name_prefix" {
   type    = string #string, any, number, bool, list, map, set, object, tuple
   default = "my_iam_user"
@@ -5,11 +14,11 @@ variable "iam_user_name_prefix" {
 
 provider "aws" {
   region  = "us-east-1"
-  version = "~> 3.18.0"
 }
 
 //Multiple users
 resource "aws_iam_user" "my_iam_users" {
-  count = 1
+  count = 2
   name  = "${var.iam_user_name_prefix}_${count.index}"
 }
+
