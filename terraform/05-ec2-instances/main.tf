766f27056e7b24f8d4c89e724e68c7b52d1e4217
llStrevensll <anguelr12@hotmail.com>
2021-05-09 18:30:08 -0500

    http server - ec2

--- terraform/05-ec2-instances/main.tf
+++ terraform/05-ec2-instances/main.tf
@@ -7,6 +7,10 @@ terraform {
   }
 }
 
+variable "aws_key_pair" {
+  default = "~/.aws/aws_keys/default-ec2.pem"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -18,6 +22,13 @@ resource "aws_security_group" "http_server_sg" {
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -42,4 +53,21 @@ resource "aws_instance" "http_server" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.http_server_sg.id]
   subnet_id              = "subnet-246ee869"
+
+  
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",
+      "sudo service httpd start",
+      "echo Welcome to  - VirtualServer is at ${self.public_dns} | sudo tee /var/www/html/index.html"
+    ]
+  }
 }
