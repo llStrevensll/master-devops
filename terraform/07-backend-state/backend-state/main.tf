596f9b5febb8ac553237dbcb1f86d131da716ecf
llStrevensll <anguelr12@hotmail.com>
2021-05-11 20:55:33 -0500

    backend-state in a s3 bucket

---
+++ terraform/07-backend-state/backend-state/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+//S3 bucket
+resource "aws_s3_bucket" "enterprise_backend_state" {
+    bucket = "dev-applications-backend-state-strevens-abc"
+
+    lifecycle {
+        prevent_destroy = true
+    }
+
+    versioning {
+        enabled = true
+    }
+
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+
+}
+
+//Locking - Dynamo DB
+
+resource "aws_dynamodb_table" "enterprise_backend_lock" {
+    name = "dev_application_locks"
+    billing_mode = "PAY_PER_REQUEST"
+
+    hash_key = "LockID"
+    
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+
+}
---
+++ terraform/07-backend-state/users/main.tf
@@ -0,0 +1,25 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+        bucket = "dev-applications-backend-state-strevens-abc"
+        #key = "07-backend-state-users-dev"
+        key = "dev/07-backend-state/users/backend-state"
+        region = "us-east-1"
+        dynamodb_table = "dev_application_locks"
+        encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_user" "my_iam_user" {
+    name = "my_iam_user_abc"
+}
