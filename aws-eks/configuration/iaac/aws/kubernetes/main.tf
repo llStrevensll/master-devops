2abaddd461fb65fa73acf361641a3605ef741ce8
llStrevensll <anguelr12@hotmail.com>
2021-06-10 07:11:14 -0500

    aws-eks-terraform-azuredevops

---
+++ aws-eks/configuration/iaac/aws/kubernetes/main.tf
@@ -0,0 +1,91 @@
+# aws --version
+# aws eks --region us-east-1 update-kubeconfig --name strevens-cluster
+# Uses default VPC and Subnet. Create Your Own VPC and Private Subnets for Prod Usage.
+# terraform-backend-state-strevens-123
+# arn:aws:s3:::terraform-backend-state-strevens-123
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket ="mybucket"  #Will be overridden from build
+    key = "path/to/my/key"  # Will be overridden from build
+    region = "us-east-1"
+  }
+}
+
+#subnet-246ee869
+#subnet-27404519
+
+resource "aws_default_vpc" "default" {
+  
+}
+
+
+
+data "aws_subnet_ids" "subnets" {
+  vpc_id = aws_default_vpc.default.id
+}
+
+data "aws_eks_cluster" "cluster" {
+    name = module.strevens-cluster.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+    name = module.strevens-cluster.cluster_id
+}
+
+provider "kubernetes" {
+  host = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+ // version                = "~> 1.9"
+}
+
+module "strevens-cluster" {
+  source = "terraform-aws-modules/eks/aws"
+  cluster_name = "strevens-cluster"
+  cluster_version = "1.17"
+  subnets         = ["subnet-246ee869", "subnet-27404519"] #CHANGE # Donot choose subnet from us-east-1e
+  #subnets = data.aws_subnet_ids.subnets.ids
+  vpc_id          = aws_default_vpc.default.id
+  #vpc_id         = "vpc-1234556abcdef"
+
+  node_groups = [
+    {
+      instance_type = "t2.micro"
+      max_capacity  = 5
+      desired_capacity = 3
+      min_capacity  = 3
+    }
+  ]
+}
+
+# We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
+# ServiceAccount needs permissions to create deployments 
+# and services in default namespace
+resource "kubernetes_cluster_role_binding" "example" {
+  metadata {
+    name = "fabric8-rbac"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "default"
+    namespace = "default"
+  }
+}
+
+# Needed to set the default region
+provider "aws" {
+  region  = "us-east-1"
+}
