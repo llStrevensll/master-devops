eca9fde61cba5b8e4b1931d777eaa7e47979966b
llStrevensll <anguelr12@hotmail.com>
2021-09-02 09:58:21 -0500

    bash sh to container, kubernetes changes

--- kubernetes-project/01-hello-world-rest-api/src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
+++ kubernetes-project/01-hello-world-rest-api/src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,6 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class RestfulWebServicesApplicationTests {
 
+	String message = "Hello World";
+	MessageUtil messageUtil = new MessageUtil(message);
+	
 	@Test
 	public void contextLoads() {
 	}
